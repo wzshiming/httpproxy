fa148b08310c91e99f9179b96971cb84a825e187
wzshiming <wzshiming@foxmail.com>
2019-06-14 00:39:48 +0800

    add user and pwd

--- cmd/httpproxy/main.go
+++ cmd/httpproxy/main.go
@@ -8,11 +8,19 @@ import (
 )
 
 var address string
+var username string
+var password string
 
 func init() {
 	flag.StringVar(&address, "a", ":8080", "listen on the address")
+	flag.StringVar(&username, "u", "", "username")
+	flag.StringVar(&password, "p", "", "password")
 }
 
 func main() {
-	http.ListenAndServe(address, &httpproxy.ProxyHandler{})
+	ph := &httpproxy.ProxyHandler{}
+	if username != "" {
+		ph.Authentication = httpproxy.BasicAuth(username, password)
+	}
+	http.ListenAndServe(address, ph)
 }
